fe30afbb106fafbdd4057f1b42b4fb22d604def8
Kan-Ru Chen (陳侃如) <kanru@kanru.info>
2012-10-30 18:57:58 +0800

    Add chewing_dso.cpp

---
+++ src/chewing_dso.cpp
@@ -0,0 +1,56 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include <dlfcn.h>
+
+#include "chewing_dso.h"
+
+#define LIBCHEWING_DSO "libchewing.so.3.0.1"
+
+bool
+chewing_dso_open(struct chewing_pfns* pfns)
+{
+  void *h;
+
+  h = pfns->_handle = dlopen(LIBCHEWING_DSO, RTLD_LAZY);
+  if (!h)
+    return false;
+
+#define LOAD_SYMBOL(sym)				\
+  do {							\
+    pfns->sym = (__typeof__ pfns->sym)dlsym(h, #sym);	\
+    if (!pfns->sym) {					\
+      dlclose(h);					\
+      return false;					\
+    }							\
+  } while(0)
+
+  LOAD_SYMBOL(chewing_new);
+  LOAD_SYMBOL(chewing_delete);
+  LOAD_SYMBOL(chewing_handle_Default);
+  LOAD_SYMBOL(chewing_handle_Enter);
+  LOAD_SYMBOL(chewing_handle_Down);
+  LOAD_SYMBOL(chewing_handle_Esc);
+  LOAD_SYMBOL(chewing_commit_Check);
+  LOAD_SYMBOL(chewing_commit_String);
+  LOAD_SYMBOL(chewing_buffer_Check);
+  LOAD_SYMBOL(chewing_buffer_Len);
+  LOAD_SYMBOL(chewing_buffer_String);
+  LOAD_SYMBOL(chewing_zuin_Check);
+  LOAD_SYMBOL(chewing_zuin_String);
+  LOAD_SYMBOL(chewing_cand_TotalPage);
+  LOAD_SYMBOL(chewing_cand_CurrentPage);
+  LOAD_SYMBOL(chewing_cand_Enumerate);
+  LOAD_SYMBOL(chewing_cand_hasNext);
+  LOAD_SYMBOL(chewing_cand_String);
+}
+
+void
+chewing_dso_close(struct chewing_pfns* pfns)
+{
+  if (pfns->_handle)
+    dlclose(pfns->_handle);
+}
---
+++ src/chewing_dso.h
@@ -0,0 +1,40 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef CHEWING_DSO_H
+#define CHEWING_DSO_H
+
+typedef struct ChewingContext ChewingContext;
+
+struct chewing_pfns {
+  void* _handle;
+  ChewingContext* (*chewing_new)();
+  void (*chewing_delete)(ChewingContext* ctx);
+
+  int (*chewing_handle_Default)(ChewingContext* ctx, int key);
+  int (*chewing_handle_Enter)(ChewingContext* ctx);
+  int (*chewing_handle_Down)(ChewingContext* ctx);
+  int (*chewing_handle_Esc)(ChewingContext* ctx);
+
+  int (*chewing_commit_Check)(ChewingContext* ctx);
+  char* (*chewing_commit_String)(ChewingContext* ctx);
+  int (*chewing_buffer_Check)(ChewingContext* ctx);
+  int (*chewing_buffer_Len)(ChewingContext* ctx);
+  char* (*chewing_buffer_String)(ChewingContext* ctx);
+  int (*chewing_zuin_Check)(ChewingContext* ctx);
+  char* (*chewing_zuin_String)(ChewingContext* ctx, int* zuin_count);
+
+  int (*chewing_cand_TotalPage)(ChewingContext* ctx);
+  int (*chewing_cand_CurrentPage)(ChewingContext* ctx);
+  void (*chewing_cand_Enumerate)(ChewingContext* ctx);
+  int (*chewing_cand_hasNext)(ChewingContext* ctx);
+  char* (*chewing_cand_String)(ChewingContext* ctx);
+};
+
+bool chewing_dso_open(struct chewing_pfns* pfns);
+void chewing_dso_close(struct chewing_pfns* pfns);
+
+#endif
